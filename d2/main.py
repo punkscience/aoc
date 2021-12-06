10857dbb67e6676f0d7bffe5858de61e12d81af1
Darryl Wright <darryl@punkscience.ca>
2021-12-06 10:48:42 -0400

    Day 2 complete.

--- d2/main.py
+++ d2/main.py
@@ -1,28 +1,27 @@
+f = open("input.txt", "r")
+lines = f.readlines()
 
-def ReadData( filename ):
-    readings = []
+depth = 0
+hpos = 0
+aim = 0
 
-    with open( filename, "r" ) as f:
-        lines = f.readlines()
-        
-        for line in lines:
-            readings.append( int( line.strip() ) )
+for line in lines:
+    words = line.split()
+    command = words[0]
+    val = int( words[1] )
 
-    return readings
+    if command == 'forward':
+        hpos += val
+        depth += (aim * val)
+    elif command == 'down':
+        #depth += val
+        aim += val
+    elif command == 'up':
+        #depth -= val
+        aim -= val
+    else:
+        print( "Unknown command.")
 
+result = depth * hpos
 
-data = ReadData( '../input.txt' )
-
-previous = data[0]
-increase_count = 0
-frame_index = 0
-
-for sample in data:
-    if sample > previous:
-        increase_count = increase_count + 1
-    previous = sample
-
-print( f'There have been {increase_count} progressions.')
-
-
-
+print( f"Result: {result}" )
