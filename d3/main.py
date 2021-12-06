6610a34ca9f16fa9765052173625d5c6f7558563
Darryl Wright <darryl@punkscience.ca>
2021-12-06 14:46:44 -0400

    Work in progress on d3b

--- d3/main.py
+++ d3/main.py
@@ -3,6 +3,62 @@ numbers = f.readlines()
 
 EPSILON = 0
 GAMMA = 1
+OXYGEN = 3
+CO2 = 4
+
+def detectMostCommonValue( numbers, position ):
+    bits = 0
+    numberLen = len(numbers)
+
+    for line in numbers:
+        number = line.strip()
+        
+        if number[position] == '1':
+            bits += 1
+
+    if bits >= (numberLen/2):
+        return 1
+    
+    return 0
+
+
+def calcLSRate( numbers, method ):
+    numberLen = len(numbers[0].strip())
+    resultList = numbers.copy()
+
+    # Test against all the criteria
+    for i in range(numberLen):
+        criteria = detectMostCommonValue(numbers, i)
+
+        # Flip the meaning for CO2
+        if method == CO2:
+            if criteria == 1:
+                criteria = 0
+            else:
+                criteria = 1
+
+        resultList = filter(resultList, criteria, i )
+
+        if len(resultList) == 1:
+            return resultList[0]
+
+    return calcLSRate( resultList, method )
+
+
+def filter( numbers, criteria, position ):
+    resultList = numbers.copy()
+
+    for line in resultList:
+        number = line.strip()
+
+        print( f"number: {number[position]} criteria: {criteria}")
+        if number[position] != str(criteria):
+            print( f"Removed {line}")
+            resultList.remove(line)
+
+    return resultList
+
+
 
 def calcRate(numbers, method):
     resultBinary = []
@@ -39,14 +95,18 @@ def calcRate(numbers, method):
 
     print( f"Result: {resultString} ({int(resultString,2)})" )
 
-    return int(resultString, 2)
+    return resultString
             
-gammaRate = calcRate(numbers, GAMMA)
-epsilonRate = calcRate(numbers, EPSILON)
+gammaRate = int( calcRate(numbers, GAMMA), 2 )
+epsilonRate = int( calcRate(numbers, EPSILON), 2 )
+oxygenRate = int( calcLSRate( numbers, OXYGEN ), 2)
+c02Rate = int( calcLSRate( numbers, CO2 ), 2)
 
 result = gammaRate * epsilonRate
+lifeSupport = oxygenRate * c02Rate
 
 print( f"Power Consumption: {result}")
+print(f"Life Support: {lifeSupport}")
 
 
 
